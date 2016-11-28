2b877fb3684ce57f713ed1293067cdc38f3d8830
Zhou Lianjian <1126185240@qq.com>
2016-11-28 06:40:20 -0600

    optimize

--- jsonp.js
+++ jsonp.js
@@ -1,3 +1,4 @@
+//jsonp with Promise
 (function(window){
   function isObject(thing){
     return Object.prototype.toString.call(thing) === '[object Object]'
@@ -8,35 +9,42 @@
       return key + '=' + obj[key]
     }).join('&')
   }
-  function setUpUrl(url, params){
-    var cbQS = jsonp.cbQueryString + '=' + jsonDataCatcher
+  
+  var cbId = 0
+  
+  var jsonp = function(url, params, remoteCb, timeout){
+    var thisId = '_jsonp' + cbId++
+    remoteCb = remoteCb || jsonp.cbQueryString
     params = serialize(params)
     
     if(url.indexOf('?') === -1) url += '?'
     if(params.length > 0) url += '&' + params
-    return [url, cbQS].join('&')
-  }
-  
-  var jsonpBus = {}
-  var jsonp = function(url, params){
-    var script = document.createElement('script')
-    script.src = setUpUrl(url, params)
-    console.log(script.src)
-    document.head.appendChild(script)
+    url += '&' + remoteCb + '=' + thisId
+    
+    var script = document.createElement('script'),
+        head = document.getElementsByTagName('head')[0] || document.head
+    script.src = url
+    head.appendChild(script)
     return new Promise(function(resolve, reject){
-      jsonpBus.onData = function(data){
+      var timer = setTimeout(function(){
+        reject('jsonp Timeout.')
+        window[thisId] = null
+        head.removeChild(script)
+      }, timeout || 30000)
+      
+      window[thisId] = function(data){
+        if(timer) clearTimeout(timer)
         resolve(data)
-        document.head.removeChild(script)
+        window[thisId] = null
+        head.removeChild(script)
       }
       script.onerror = function(err){
+        if(timer) clearTimeout(timer)
         reject(err)
-        document.head.removeChild(script)
+        window[thisId] = null
+        head.removeChild(script)
       }
     })
-  }
-  var jsonDataCatcher = 'jsonp' + Date.now()
-  window[jsonDataCatcher] = function(data){
-      jsonpBus.onData(data)
   }
   jsonp.cbQueryString = 'callback'
   jsonp.setRemoteCb = function(cbQueryString){
